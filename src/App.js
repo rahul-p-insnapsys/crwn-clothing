8360079bd25fadca61f5abf7424dda2c487f2292
rahul-p-insnapsys <rahul-p-insnapsys@users.noreply.github.com>
2020-09-18 18:10:28 +0530

    update app with shop page, add collection preview item and collection item component

--- src/App.js
+++ src/App.js
@@ -1,46 +1,47 @@
 import React from "react";
 import "./App.css";
-import HomePage from "./pages/homepage/homepage.component";
 import { Switch, Route, Link } from "react-router-dom";
+import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
 
-const HatsPage = (props) => {
-  console.log(props);
-  return (
-    <div>
-      <Link to="/topic">Topic</Link>
-      <button
-        onClick={() => {
-          props.history.push("/topic");
-        }}
-      >
-        Topic
-      </button>
-      <h1>HATS PAGE</h1>
-    </div>
-  );
-};
+// const HatsPage = (props) => {
+//   console.log(props);
+//   return (
+//     <div>
+//       <Link to="/topic">Topic</Link>
+//       <button
+//         onClick={() => {
+//           props.history.push("/topic");
+//         }}
+//       >
+//         Topic
+//       </button>
+//       <h1>HATS PAGE</h1>
+//     </div>
+//   );
+// };
 
-const TopicList = (props) => {
-  console.log(props);
-  return (
-    <div>
-      <h1>TopicList page</h1>
-      <h3>path: {props.match.path}</h3>
-      <h3>url: {props.match.url}</h3>
-    </div>
-  );
-};
+// const TopicList = (props) => {
+//   console.log(props);
+//   return (
+//     <div>
+//       <h1>TopicList page</h1>
+//       <h3>path: {props.match.path}</h3>
+//       <h3>url: {props.match.url}</h3>
+//     </div>
+//   );
+// };
 
-const TopicListDetail = (props) => {
-  console.log(props);
-  return (
-    <div>
-      <h1>TopicListDetail page</h1>
-      <h3>path: {props.match.path}</h3>
-      <h3>url: {props.match.url}</h3>
-    </div>
-  );
-};
+// const TopicListDetail = (props) => {
+//   console.log(props);
+//   return (
+//     <div>
+//       <h1>TopicListDetail page</h1>
+//       <h3>path: {props.match.path}</h3>
+//       <h3>url: {props.match.url}</h3>
+//     </div>
+//   );
+// };
 
 function App() {
   return (
@@ -48,9 +49,7 @@ function App() {
       <Switch>
         <Route exact={true} path="/" component={HomePage} />{" "}
         {/*exact means path should exactly like specified one it has two value true or false, default is true*/}
-        <Route path="/hats" component={HatsPage} />
-        <Route path="/topic" component={TopicList} />
-        <Route path="/topics/:topicId" component={TopicListDetail} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
